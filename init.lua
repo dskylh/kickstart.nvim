37b4d73a3048e18418f1b061dff9fa3d90abefd6
Taha Ugurcan Ongel <dskylh@protonmail.com>
2024-01-03 16:02:10 +0300

    update

--- init.lua
+++ init.lua
@@ -618,7 +618,6 @@ else
       end, { 'i', 's' }),
     },
     sources = {
-      { name = 'copilot',  group_index = 2 },
       { name = 'nvim_lsp', group_index = 2 },
       { name = 'luasnip',  group_index = 2 },
       { name = 'path',     group_index = 2 }
@@ -630,21 +629,6 @@ else
         ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
         symbol_map = { Copilot = "" }
       }),
-    },
-    comparators = {
-      require("copilot_cmp.comparators").prioritize,
-
-      -- Below is the default comparitor list and order for nvim-cmp
-      cmp.config.compare.offset,
-      -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-      cmp.config.compare.exact,
-      cmp.config.compare.score,
-      cmp.config.compare.recently_used,
-      cmp.config.compare.locality,
-      cmp.config.compare.kind,
-      cmp.config.compare.sort_text,
-      cmp.config.compare.length,
-      cmp.config.compare.order,
     },
   }
 
--- lua/custom/plugins/copilot-cmp.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"zbirenbaum/copilot-cmp",
-	config = function()
-		require("copilot_cmp").setup()
-	end,
-	event = { "InsertEnter", "LspAttach" },
-	fix_pairs = true,
-
-}
